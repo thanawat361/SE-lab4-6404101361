a2afd1a8623abd4fb964d174a04d85c4e3273f0c
thanawat <beemrockkung@gmail.com>
2024-03-09 19:13:40 +0700

    IndexPage

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,20 @@
+<!-- IndexPage.vue -->
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'HtmlComponent',
+  data() {
+    return {
+      title: 'Vue is awesome.',
+    }
+  },
+}
+</script>
+
+<style scoped></style>
